55715269ae65f87c2ab125deaad150ead343b93d
sak5010 <38302831+sak5010@users.noreply.github.com>
2020-08-31 18:06:58 +0900

    기수정렬 구현
    기수정렬은 시간복잡도가 O(DN)인 정렬 알고리즘 입니다. (D는 가장 큰 데이터의 자릿수)
    기수정렬은 계수정렬보다 약간 느리지만, 숫자가 매우 큰 상황에서도 사용할 수 있습니다. (계수정렬과 마찬가지로 별도의 데이터 공간이 필요함)

---
+++ radix_sort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#define MAX 10000
+
+void radixSort(int* a, int n) {
+	int res[MAX], 	maxValue = 0;
+	int exp = 1;
+	for (int i = 0; i < n; i++) {
+		if (a[i] > maxValue) maxValue = a[i];	
+	}
+	while (maxValue / exp > 0) { // 1의 자리부터 계산 
+		int bucket[10] = { 0 };
+		for (int i = 0; i < n; i++) {
+			bucket[a[i] / exp % 10]++; // 자릿수 배열 처리	
+		}
+		for (int i = 1; i < 10; i++) {
+			bucket[i] += bucket[i - 1];	// 누적 계산 
+		}
+		for (int i = n - 1; i >= 0; i--) {
+			res[--bucket[a[i] / exp % 10]] = a[i]; // 같은 자릿수 까지는 순서를 유지 
+		}
+		for (int i = 0; i < n; i++) {
+			a[i] = res[i]; // 기본 배열 갱신
+		}
+		exp *= 10;
+	}
+}
+
+int main(void) {
+	int a[MAX];
+	int i, n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+	radixSort(a, n);
+	for (int i = 0; i < n; i++) printf("%d ", a[i]);	
+	return 0;
+}
