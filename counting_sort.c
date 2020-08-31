f314dd5bc69d06d1267a40714f0bb6dd797f917a
sak5010 <38302831+sak5010@users.noreply.github.com>
2020-08-31 17:17:42 +0900

    기수정렬 구현

---
+++ counting_sort.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define MAX_VALUE 10001
+
+int n, m;
+int a[MAX_VALUE];
+
+int main(void) {
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &m);
+		a[m]++;	
+	}
+	for (int i = 0; i < MAX_VALUE; i++) {
+		while (a[i] != 0) {
+			printf("%d ", i);
+			a[i]--;
+		}
+	}
+	return 0;
+}
